277b263f5082825152dbddd7b6b176183e36d80d
Shahilcoder <shahilcoder@gmail.com>
2021-10-20 20:42:30 +0530

    Add top cards

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -5,10 +5,32 @@ const Content = () => {
     return (
         <div class="content">
             <div class="cards">
-                <div>Hey</div>
-                <div>Yo</div>
-                <div>I am</div>
-                <div>Cool</div>
+                <div>
+                    <span class="dots">. . .</span>
+                    Total Orders
+                    <div class="sm">something</div>
+                    <div class="number">3456</div>
+                </div>
+                <div>
+                    <span class="dots">. . .</span>
+                    Bot Orders
+                    <div class="sm">something</div>
+                    <div class="number">
+                        2345 <span class="attract">+34%</span>
+                    </div>
+                </div>
+                <div>
+                    <span class="dots">. . .</span>
+                    Ongoing Conversation
+                    <div class="sm">something</div>
+                    <div class="number">2341</div>
+                </div>
+                <div>
+                    <span class="dots">. . .</span>
+                    Orders Completed
+                    <div class="sm">something</div>
+                    <div class="number">1598</div>
+                </div>
             </div>
         </div>
     )
